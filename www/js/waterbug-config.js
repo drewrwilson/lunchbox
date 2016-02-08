7eab4312d4d5f65a5660d40e8959033e41de1222
drewrwilson <drew.r.wilson@gmail.com>
2016-02-08 15:11:38 -0500

    customized for netposi podcast

--- www/js/waterbug-config.js
+++ www/js/waterbug-config.js
@@ -6,19 +6,19 @@ var elementPadding = 40; // padding around the logo and credit text
 // the name of the logo object should match the value of the corresponding radio button in the HTML.
 var logos = {
     'lunchbox': {
-        whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
-        blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
-        w: 100, // width of logo
-        h: 80, // height of logo
-        display: 'Lunchbox'
-    },
-    'socializr': {
-        whitePath: '../img/icon-socializr-white.svg',
-        blackPath: '../img/icon-socializr-black.svg',
-        w: 150,
-        h: 51,
-        display: 'Socializr'
-    }
+        whitePath: '../img/netposi/social-media-icon-white-only-SMALL-700Xx350.png', // path to white logo
+        blackPath: '../img/netposi/social-media-icon-black-only-SMALL-700Xx350.png', // path to black logo
+        w: 200, // width of logo
+        h: 100, // height of logo
+        display: 'NetPosi'
+    }//,
+    // 'socializr': {
+    //     whitePath: '../img/icon-socializr-white.svg',
+    //     blackPath: '../img/icon-socializr-black.svg',
+    //     w: 300,
+    //     h: 102,
+    //     display: 'Socializr'
+    // }
 };
 
 // logo opacity for colors
@@ -45,7 +45,7 @@ var copyrightOptions = {
         photographerRequired: false, // require a photographer
         sourceRequired: false, // require a source
         source: orgName, // How the source should appear on the image, e.g. 'NPR'
-        display: orgName, // How the option will appear in the dropdown menu   
+        display: orgName, // How the option will appear in the dropdown menu
     },
     'freelance': {
         showPhotographer: true,
@@ -53,7 +53,7 @@ var copyrightOptions = {
         photographerRequired: true,
         sourceRequired: false,
         source: freelanceString,
-        display: 'Freelance' 
+        display: 'Freelance'
     },
     'ap': {
         showPhotographer: true,
@@ -61,7 +61,7 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: false,
         source: 'AP',
-        display: 'AP' 
+        display: 'AP'
     },
     'getty': {
         showPhotographer: true,
@@ -69,7 +69,7 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: false,
         source: 'Getty Images',
-        display: 'Getty' 
+        display: 'Getty'
     },
     'thirdParty': {
         showPhotographer: true,
@@ -77,7 +77,7 @@ var copyrightOptions = {
         photographerRequired: false,
         sourceRequired: true,
         source: '',
-        display: 'Third Party/Courtesy' 
+        display: 'Third Party/Courtesy'
     }
 }
 
